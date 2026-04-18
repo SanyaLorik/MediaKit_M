3a8209cc9d1a4b60216bee435ac5dc8449ddc154
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-04-18 22:36:26 +0300

    Start develop applyer

---
+++ Hyper_Casual/Prefabs/Skin Changer/Scripts/SkinApplyer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MediaKit_M.SkinChanger
+{
+    public class SkinApplyer : MonoBehaviour
+    {
+        [Header("Buttons")]
+        [SerializeField] private Button _boughtButton;
+        [SerializeField] private Button _selectButton;
+        [SerializeField] private Button _canSelectButton;
+    }
+}
