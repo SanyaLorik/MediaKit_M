51743aea6363b6eaf1ad8c66f7ae4f7c7363a983
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-04-18 22:51:20 +0300

    Add skin applyer

--- Hyper_Casual/Prefabs/Skin Changer/Scripts/SkinApplyer.cs
+++ Hyper_Casual/Prefabs/Skin Changer/Scripts/SkinApplyer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace MediaKit_M.SkinChanger
 {
@@ -9,5 +10,36 @@ namespace MediaKit_M.SkinChanger
         [SerializeField] private Button _boughtButton;
         [SerializeField] private Button _selectButton;
         [SerializeField] private Button _canSelectButton;
+
+        [Inject] private ISkinSaveLoader _loader;
+
+        private void OnEnable()
+        {
+            _boughtButton.onClick.AddListener(OnBuy);
+            _boughtButton.onClick.AddListener(OnSelect);
+            _boughtButton.onClick.AddListener(OnCanSelect);
+        }
+
+        private void OnDisable()
+        {
+            _boughtButton.onClick.RemoveListener(OnBuy);
+            _boughtButton.onClick.RemoveListener(OnSelect);
+            _boughtButton.onClick.RemoveListener(OnCanSelect);
+        }
+
+        private void OnBuy()
+        {
+
+        }
+
+        private void OnSelect()
+        {
+
+        }
+
+        private void OnCanSelect()
+        {
+
+        }
     }
 }
